04d2269cd43c67898127f1dbfc9d8db059895b03
soufianchk <chakkouri.soufian@ump.ac.ma>
2023-10-16 14:27:38 -0700

    update

--- main.h
+++ main.h
@@ -5,5 +5,19 @@
 #include "stdlib.h"
 
 int print_s(va_list arg);
+int print_c(va_list arg);
+int print_percent(va_list arg);
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
