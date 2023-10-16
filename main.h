4c69e30cd63cf4dcd36b242f9757beb663c73048
soufianchk <chakkouri.soufian@ump.ac.ma>
2023-10-16 15:47:04 -0700

    update

--- main.h
+++ main.h
@@ -7,5 +7,33 @@
 int print_s(va_list arg);
 int print_c(va_list arg);
 int print_percent(va_list arg);
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int get_flags(const char *format, int *i);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 #endif
